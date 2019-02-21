b2a71801f5ffc3ecc653d2045b66ca61cbc2c753
Marshall Bowles <marshall@marshallbowles.com>
2019-02-20 17:12:54 -0800

    Add stub pages and fix menu links
    The build.py script is refactored so that it's easy to generate
    multiple pages. The stubs are for the pages listed in the menu
    data.

--- build.py
+++ build.py
@@ -6,7 +6,7 @@ import sass
 
 
 Menu = namedtuple('Menu', ['name', 'route'])
-menu_items = [
+MENU_ITEMS = [
     Menu('Getting started', 'getting-started.html'),
     Menu('Syntax', 'syntax.html'),
     Menu('Tools', 'tools.html'),
@@ -27,22 +27,30 @@ def build_styles():
 def build_pages():
     """Generate html content."""
     banner = render_page('templates/banner.html.jinja')
-    index = render_page(
-        'templates/index.html.jinja',
+    generate_page_content(banner, 'Home', 'index.html')
+    generate_page_content(banner, 'Getting started', 'getting-started.html')
+    generate_page_content(banner, 'Syntax', 'syntax.html')
+    generate_page_content(banner, 'Tools', 'tools.html')
+    generate_page_content(banner, 'Documentation', 'documentation.html')
+
+
+def generate_page_content(banner, page_name, filename):
+    content = render_page(
+        'templates/base.html.jinja',
         {
             "banner": banner,
             "menu": render_page(
                 'templates/menu.html.jinja',
                 {
-                    "current_page": "Home",
-                    "menu_items": menu_items,
+                    "current_page": page_name,
+                    "menu_items": MENU_ITEMS,
                 }
             ),
-            "content": render_page('templates/home.html.jinja'),
+            "content": render_page('templates/{}.jinja'.format(filename)),
         }
     )
-    with open('index.html', 'w') as f:
-        f.write(htmlmin.minify(index))
+    with open(filename, 'w') as f:
+        f.write(htmlmin.minify(content))
 
 
 def render_page(path, args={}):
